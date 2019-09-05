d1dd84a22e54fe06866eb7d368f8b0e6d48ecd0c
dkscodnd <48791625+dkscodnd@users.noreply.github.com>
2019-09-05 20:46:48 +0900

    Add files via upload

---
+++ act.c
@@ -0,0 +1,13 @@
+//int act(int act_num) {
+//	int target;
+//	switch (act_num) {
+//	case 0: // BANG
+//		printf("누구에게 쏘겠습니까?\n");
+//		bang(target);
+//	case 1: // 피하기
+//	case 2: // 맥주
+//	case 3: // 캣 벌로우 (옆에 있는 사람 카드 삭제)
+//		;
+//	}
+//
+//}
---
+++ bang.c
@@ -0,0 +1,3 @@
+//int bang(int target) {
+//
+//}
